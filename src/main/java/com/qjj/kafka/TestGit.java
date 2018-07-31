adb58b496b6cbb52018838249cb47ccab6cd4e07
quanjiajin2018 <quanjiajin@163.com>
2018-07-31 22:55:00 +0800

    新增测试工程

--- src/main/java/com/qjj/kafka/TestGit.java
+++ src/main/java/com/qjj/kafka/TestGit.java
@@ -4,7 +4,7 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("12233333333333");
         System.out.println("1223");
-        System.out.println("123");
+        System.out.println("1534545");
 
     }
 }
