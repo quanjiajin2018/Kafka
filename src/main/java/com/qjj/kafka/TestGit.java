065abe65df83b28b9b8ddcc0c8e9f9ad2b37b65b
quanjiajin2018 <quanjiajin@163.com>
2018-07-31 22:43:25 +0800

    删除

--- src/main/java/com/qjj/kafka/TestGit.java
+++ src/main/java/com/qjj/kafka/TestGit.java
@@ -2,9 +2,6 @@ package com.qjj.kafka;
 
 public class TestGit {
     public static void main(String[] args) {
-        System.out.println("1223");
-        System.out.println("123");
-        System.out.println("3334");
-        System.out.println("10086");
+        System.out.println("12233333333333");
     }
 }
