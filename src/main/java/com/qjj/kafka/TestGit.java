4500b716940f51332d1794f414210f9fca25cfea
quanjiajin2018 <quanjiajin@163.com>
2018-07-31 22:57:29 +0800

    提交到本地

--- src/main/java/com/qjj/kafka/TestGit.java
+++ src/main/java/com/qjj/kafka/TestGit.java
@@ -2,9 +2,8 @@ package com.qjj.kafka;
 
 public class TestGit {
     public static void main(String[] args) {
-        System.out.println("12233333333333");
-        System.out.println("1223");
-        System.out.println("123");
+    	System.out.println("修改代码");
+        
 
     }
 }
